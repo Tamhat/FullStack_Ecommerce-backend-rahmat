636905f71b748e6d2accccb2af19591954389547
Rahmat ullah badsha <87254817+Tamhat@users.noreply.github.com>
2023-08-21 08:06:17 +0100

    Update2 server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(path.resolve(), "/build")));
+app.use(express.static(path.join("https://github.com/Tamhat/Render-Ecommerce-Rahmat-Client/src/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,7 +31,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use("*", (req, res) => {
-res.sendFile(path.join(path.resolve(), "/build/index.html"));
+res.sendFile(path.join("https://github.com/Tamhat/Render-Ecommerce-Rahmat-Client/src/build/index.html"));
 });
 
 //PORT
