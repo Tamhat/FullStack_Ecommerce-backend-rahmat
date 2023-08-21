aa46fcd96f1a748dff3a3fe9dae36238ace9dca9
Rahmat ullah badsha <87254817+Tamhat@users.noreply.github.com>
2023-08-21 07:25:21 +0100

    Update with file change again server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(path.resolve(), "../client/build")));
+app.use(express.static(path.join(path.resolve(), "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,7 +31,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use("*", (req, res) => {
-res.sendFile(path.join(path.resolve(), "../client/build/index.html"));
+res.sendFile(path.join(path.resolve(), "./client/build/index.html"));
 });
 
 //PORT
