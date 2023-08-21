c9fc0a3bfc9923a78b9fa7da42a9c6b756584271
Rahmat ullah badsha <87254817+Tamhat@users.noreply.github.com>
2023-08-21 07:58:46 +0100

    Update reagain server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(path.resolve(), "https://github.com/Tamhat/Render-Ecommerce-Rahmat-Client/build")));
+app.use(express.static(path.join(path.resolve(), "/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,7 +31,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use("*", (req, res) => {
-res.sendFile(path.join(path.resolve(), "https://github.com/Tamhat/Render-Ecommerce-Rahmat-Client/build/index.html"));
+res.sendFile(path.join(path.resolve(), "/build/index.html"));
 });
 
 //PORT
